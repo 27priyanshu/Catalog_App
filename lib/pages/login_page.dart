79de116685b0c5932d7330c1a5a1e583ff0e4c8e
Priyanshu <2003priyanshusingh@gmail.com>
2023-02-17 11:37:25 +0530

    day 4 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -10,11 +10,46 @@ class LoginPage extends StatelessWidget {
         child: Column(
           children: [
             Image.asset("assets/images/login_image.png"),
+            SizedBox(
+              height: 20,
+            ),
             Text(
               "Welcome",
               style: TextStyle(
                 fontSize: 22,
                 fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter User Name",
+                      labelText: "Username",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "Password",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                    child: Text("Login"),
+                    onPressed: (() {
+                      print("Hy priyanshu singh");
+                    }),
+                  )
+                ],
               ),
             )
           ],
