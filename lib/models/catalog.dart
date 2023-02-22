2b9669781718092d7417b2a30ec70c40d60c4d37
Priyanshu <2003priyanshusingh@gmail.com>
2023-02-22 11:48:27 +0530

    day10 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -6,17 +6,17 @@ class Item {
   final String color;
   final String image;
 
-  Item({this.id, this.name, this.desc, this.price, this.color, this.image});
+  Item(this.id, this.name, this.desc, this.price, this.color, this.image);
 }
 
-final product = [
-  Item(
-    id: "priyanshu001",
-    name: "iphone12",
-    desc: "newly launched iphone12",
-    price: "999",
-    color: "#33505a",
-    image:
-        "https://m.media-amazon.com/images/I/31jQ91XUDhS._SY445_SX342_QL70_FMwebp_.jpg",
-  )
-];
+// final product = [
+//   Item(
+//     id: "priyanshu001",
+//     name: "iphone12",
+//     desc: "newly launched iphone12",
+//     price: 999,
+//     color: "#33505a",
+//     image:
+//         "https://m.media-amazon.com/images/I/31jQ91XUDhS._SY445_SX342_QL70_FMwebp_.jpg",
+//   )
+// ];
