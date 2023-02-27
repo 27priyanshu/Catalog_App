bac8542a03a294ef6541118efdcfb97c35ab5599
Priyanshu <2003priyanshusingh@gmail.com>
2023-02-27 09:49:03 +0530

    day17 completed

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_catalog/utils/routes.dart';
 import "package:velocity_x/velocity_x.dart";
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -44,6 +46,11 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     // final dummyList = List.generate(10, (index) => CatalogModel.items[0]);
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: MyTheme.darkBluishColor,
+        onPressed: () => Navigator.pushNamed(context, MyRoutes.cartRoute),
+        child: Icon(CupertinoIcons.cart),
+      ),
       backgroundColor: MyTheme.creamColor,
       body: SafeArea(
         child: Container(
--- lib/widgets/home_widgets/catalog_list.dart
+++ lib/widgets/home_widgets/catalog_list.dart
@@ -65,7 +65,7 @@ class CatalogItem extends StatelessWidget {
                             backgroundColor: MaterialStateProperty.all(
                                 MyTheme.darkBluishColor),
                             shape: MaterialStateProperty.all(StadiumBorder())),
-                        child: "Buy".text.make())
+                        child: "Add to cart".text.make())
                   ],
                 ).pOnly(right: 8.0)
               ]),
